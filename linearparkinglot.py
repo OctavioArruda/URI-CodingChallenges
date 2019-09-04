3d7a3e897cfcae0e894e55f0e369c93666341c1b
Octavio Arruda <octavio@maxiprod.com.br>
2019-09-04 20:55:56 -0300

    linear with test

--- linearparkinglot.py
+++ linearparkinglot.py
@@ -1,51 +1,33 @@
-motoristas = 100
-max_vagas = 100
-while(motoristas != 0 and max_vagas != 0):
-    motoristas, max_vagas = input().split()
-    lista_chegada = []
-    pilha_saida = []
-    chegada_saida = []
-    motoristas_consecutivos = []
-
-    chegada_saida = input().strip().split()
-    lista_chegada.append(int(chegada_saida[0])) # queue
-    pilha_saida.append(int(chegada_saida[1])) # stack
-    motoristas_consecutivos.append(int(chegada_saida[1]))
-    for _ in range(int(motoristas)):
-        # Adicionando em duas pilhas os horários de 
-        # Chegada e de saída, respectivamente:
-
-        # Comparar se o motorista atual vai sair antes do próximo, o que faria continuar as iteraçoes,
-        # subtraindo um
-        chegada_saida_proximo_motorista = input().strip().split()
-        print(chegada_saida_proximo_motorista)
-        # Nessa lista haverão sempre o horário de motorista de saída e o de chegada do próximo
-        motoristas_consecutivos.append(int(chegada_saida_proximo_motorista[1]))
-        motoristas_consecutivos.append(int(chegada_saida_proximo_motorista[0]))
-
-        # Comparar se o primeiro a chegar é o último a sair:
-        lista_chegada.append(int(chegada_saida_proximo_motorista[0])) # queue
-        pilha_saida.append(int(chegada_saida_proximo_motorista[1])) # stack
-
-    fila_chegada = lista_chegada.reverse()
-    count = 0 
-    print(lista_chegada)
-    print(pilha_saida)
-    while (count < int(max_vagas)):
-        # Se o primeiro a chegar tentar sair antes de outro 
-        # motorista, não irá funcionar.
-
-        if (fila_chegada.pop() < pilha_saida.pop()):
-            erro = True
-        
-        if (motoristas_consecutivos[count] <= motoristas_consecutivos[count + 1]):
-            # Se um motorista sair antes de outro entrar, o número de vagas livres diminui
-            count = count - 1
+#!python
+erro = False
+while True:
+    erro = False
+    n, k = input().split()
+    if int(n) == 0 and int(k) == 0:
+        break
+    stack = []
+    for _ in range(int(n)):
+        chegada, saida = input().split()
+        if len(stack) == 0:
+            # primeiro carro
+            stack.append([chegada, saida])
+        elif int(chegada) > int(stack[-1][1]):
+            # chegou depois que o anterior saiu
+            stack.pop()                     # tira anterior
+            stack.append([chegada, saida])  # estaciona atual
+        elif int(saida) <= int(stack[-1][1]):
+            stack.append([chegada, saida])  # vai sair antes que o anterior, estaciona atual
+            # check qt de carros
+            if len(stack) > int(k):
+                erro = True
+                break
         else:
-            count = count + 1
-
-    if (erro):
-        print("Não")
+            # deu ruim
+            erro = True
+            break
+    # confere situação final
+    if len(stack) > 1 and int(stack[-1][1]) > int(stack[-2][1]) and erro == True:
+        print("Nao")
+        continue
     else:
         print("Sim")
-
