e1b36bbd8740da9df4904e38ec9bcda75ab1738d
Octavio Arruda <octavio@maxiprod.com.br>
2019-09-18 20:39:55 -0300

    1905 parcial 2 10pc

--- 1905.cpp
+++ 1905.cpp
@@ -2,9 +2,15 @@
 
 using namespace std;
 
-int dir[4][2] = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
-
-int maze[6][6];
+/* 5 x 5 maze
+0 1 2 3 4 
+x x x x x 0
+x x x x x 1 
+x x x x x 2 
+x x x x x 3
+x x x x x 4
+*/
+int maze[4][4];
 
 int value(int i, int j)
 {
@@ -13,41 +19,79 @@ int value(int i, int j)
 
 void backtrack(int i, int j, vector<int> prev)
 {
+	// when next call isn't a valid position to continue:
+	// robber's wins
 	if (maze[i][j]) return;
+	// If the path to walk is out of the range:
+	// robber's wins
 	if (!value(i, j)) return;
+	// if the last position was reached(cop's wins):
 	if (prev[5 * i + j]) return;
 
+	// Mark it as previously visited
 	prev[5 * i + j] = 1;
 
-	for (int l = 0 ; l < 4; ++l)
+	if (i + 1 < 5 && maze[i + 1][j] == 0) 
 	{
-		backtrack(i + dir[l][0], j + dir[l][1], prev);
+		// down		
+		maze[i][j] ==  1;
+		backtrack(++i, j, prev);
 	}
+	if (j + 1 < 5 && maze[i][j + 1] == 0) 
+	{
+		// right
+		maze[i][j] == 1;
+		backtrack(i, ++j, prev);
+	}
+	if (j - 1 >= 0 && maze[i][j - 1] == 0) 
+	{
+		// left
+		maze[i][j] == 1;
+		backtrack(i, --j, prev);
+	}
+	if (i - 1 >= 0 && maze[i - 1][j] == 0) 
+	{
+		// above 
+		maze[i][j] == 1;
+		backtrack(--i, j, prev);
+	}
+	if (i == 4 && j == 4) 
+	{
+		return;
+	}
+
+	return;
 }
 
 int main()
 {
     int nCasos;
-    ios_base::sync_with_stdio(0); cin.tie(0);
+    //ios_base::sync_with_stdio(0); cin.tie(0);
 	cin >> nCasos;
     vector<int> prev;
 
 	while (nCasos--)
-	{
-		for (int i = 0; i < 5; ++i) for (int j = 0; j < 5; ++j) cin >> maze[i][j];
+	{	
+		for (int i = 0; i < 5; ++i) 
+		{
+			for (int j = 0; j < 5; ++j) 
+			{					
+				cin >> maze[i][j];
+			}
+		} 
 
-		prev.assign(26, 0);
+		prev.assign(24, 0);
 
 		backtrack(0,0, prev);
 
         // If cops reach the last maze position they've won ;
 		if (prev[24])
         {
-            cout << "COPS" << endl;
+            cout << "COPS\n";
 		}
 		else
         {
-            cout << "ROBBERS" << endl;
+            cout << "ROBBERS\n";
         }
 	}
 }
