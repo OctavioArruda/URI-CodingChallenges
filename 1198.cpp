c97b1528c60eb267e8bf81d059cc12ffeb1e00cd
Octavio Arruda <octavio@maxiprod.com.br>
2019-11-11 20:11:27 -0300

    math problems

---
+++ 1198.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int a , b;
+
+    while(cin >> a >> b)
+    {
+        printf("%d\n", b - a);
+    }
+
+    return 0;
+}
